d92370acdaf03cecd2e2b2775fa63dd622b3cffa
Jason Wilson <jason.wilson2@marist.edu>
2018-01-29 17:54:58 -0500

    Soulution to Curiosity rover excersise.

---
+++ rover.py
@@ -0,0 +1,10 @@
+def calculate():
+    print("Time it takes to send a picture from Mars to Earth!!")
+    x = 34000000 / 186000
+    y = x / 60
+    print("It takes:")
+    print(x , "Seconds")
+    print("*or*")
+    print(y , "Minutes")
+    print("Type 'Calculate()' to see it happen again!")
+calculate()
