902d60f4153db55c95910e6287c16ee3550a4314
Jason Wilson <jason.wilson2@marist.edu>
2018-02-02 18:29:08 -0500

    Soulution to Mad Libs exercise.

---
+++ madlib.py
@@ -0,0 +1,11 @@
+#madlib.py
+def main():
+    print("Lets make a madlib!")
+    noun = input("Enter a noun: ")
+    verb = input("Enter a verb: ")
+    adj = input("Enter a adjective: ")
+    place = input("Enter a place: ")
+    print("Take your", adj, noun, "and", verb, "it at the",place,"!")
+    print("")
+    main()
+main()
