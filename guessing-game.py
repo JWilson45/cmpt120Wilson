d05e969665efbf61b0fcf4239f9a58ab23f09776
Jason Wilson <jason.wilson2@marist.edu>
2018-02-26 18:40:10 -0500

    Initial version of a simple guessing game.

---
+++ guessing-game.py
@@ -0,0 +1,10 @@
+answer = 'ostrich'
+
+while 1 == 1:
+    print('Thinking of an Animal')
+    print("Why not take a guess? You've got nothing better to do.")
+    if input() == answer:
+        break
+    else:
+        print('Try again')
+print('Whoop. Dee. Doo. You got it.')
